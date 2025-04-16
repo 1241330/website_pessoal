21dfc44f41d455f3b742ffd8f30d22ebc5effef5
Carlota Joana Mendes Lemos <1241330@isep.ipp.pt>
2025-04-16 22:18:20 +0100

    update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,20 +3,23 @@ import { useState } from 'react';
 import { Link } from 'react-scroll';
 
 const Navbar = () => {
-    // Estado para controlar a visibilidade do menu
     const [isOpen, setIsOpen] = useState(false);
 
-    // Função para alternar a visibilidade do menu
     const toggleMenu = () => setIsOpen(!isOpen);
-
-    // Função para fechar o menu ao clicar fora
     const closeMenu = () => setIsOpen(false);
 
     return (
-        <nav className="p-4 fixed w-full top-0 z-60 border font-mono bg-black">
-            <div className="max-w-7xl mx-auto flex items-center justify-between">
-                {/* Menu para desktop */}
-                <div className="hidden md:flex space-x-6">
+        <nav className="p-8 fixed w-full top-0 z-60 border font-mono bg-black">
+            <div className="relative max-w-7xl mx-auto flex items-center justify-between">
+                {/* Botão hambúrguer para mobile */}
+                <button className="md:hidden text-white z-50" onClick={toggleMenu}>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
+                    </svg>
+                </button>
+
+                {/* Menu centralizado para desktop */}
+                <div className="hidden md:flex space-x-6 absolute left-1/2 transform -translate-x-1/2">
                     <Link to="inicio" smooth={true} className="text-white hover:text-gray-200 cursor-pointer">Início</Link>
                     <Link to="sobre" smooth={true} className="text-white hover:text-gray-200 cursor-pointer">Sobre Mim</Link>
                     <Link to="hard-skills" smooth={true} className="text-white hover:text-gray-200 cursor-pointer">Hard Skills</Link>
@@ -24,14 +27,6 @@ const Navbar = () => {
                     <Link to="timeline" smooth={true} className="text-white hover:text-gray-200 cursor-pointer">Timeline</Link>
                     <Link to="projetos" smooth={true} className="text-white hover:text-gray-200 cursor-pointer">Projetos</Link>
                 </div>
-
-                {/* Ícone de menu hambúrguer para dispositivos móveis */}
-                <button className="md:hidden text-white" onClick={toggleMenu}>
-                    {/* Três tracinhos */}
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
-                    </svg>
-                </button>
             </div>
 
             {/* Menu mobile */}
