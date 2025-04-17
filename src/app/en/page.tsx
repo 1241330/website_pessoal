0746bd9024731d9e005f82ee4ba62201a8f41918
Carlota Joana Mendes Lemos <1241330@isep.ipp.pt>
2025-04-17 14:51:06 +0100

    update EN page

--- src/app/en/page.tsx
+++ src/app/en/page.tsx
@@ -255,7 +255,7 @@ export default function Home() {
                             target="_blank"
                             rel="noopener noreferrer"
                         >
-                            → Ver Website
+                            → View Website
                         </a>
 
                         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mt-4">
